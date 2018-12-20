58dcaf602ed01254ec2497cd929c4dfdb735b093
fairyin <fairyin@126.com>
2018-12-20 18:56:03 +0800

    优化图片列表页的排版布局。

--- resources/views/album_detail_photoes.blade.php
+++ resources/views/album_detail_photoes.blade.php
@@ -6,28 +6,74 @@
         <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport"/>
         <meta content="black" name="apple-mobile-web-app-status-bar-style"/>
         <meta content="telephone=no,email=no,adress=no" name="format-detection"/>
+        <link rel="stylesheet" href="{{ $s }}/mui/css/mui.min.css">
         <link href="{{ $s }}/lightGallery-1.6.11/lightgallery.min.css" rel="stylesheet">
-        <script src="{{ $s }}/js/jquery-1.10.2.js"></script>
-    </head>
-    <body class="home">
-        <div class="demo-gallery">
-            <ul id="lightgallery" class="list-unstyled row">
-                @foreach($photoes as $photo)
-                <li class="col-xs-6 col-sm-4 col-md-3" data-responsive="{{ $photo['preview'] }}"  data-src="{{ $photo['original'] }}" data-sub-html="">
-                    <a href="javascript:void(0);">
-                        <img class="img-responsive" src="{{ $photo['preview'] }}">
-                    </a>
-                </li>
-                @endforeach
-            </ul>
-        </div>
-        <script src="{{ $s }}/lightGallery-1.6.11/picturefill.min.js"></script>
-        <script src="{{ $s }}/lightGallery-1.6.11/lightgallery-all.min.js"></script>
-        <script src="{{ $s }}/lightGallery-1.6.11/jquery.mousewheel.min.js"></script>
-        <script type="text/javascript">
-          $(document).ready(function(){
+        <style>
+            .mui-table-view-cell img {
+                margin-bottom: 0;
+            }
+            .mui-table-view.mui-grid-view .mui-table-view-cell .mui-media-object {
+                border-top-left-radius: 8px;
+                border-top-right-radius: 8px;
+            }
+            .mui-table-view.mui-grid-view .mui-table-view-cell .mui-media-body {
+                font-size: .6rem;
+                width: 100%;
+                text-overflow: ellipsis;
+                color: #333;
+                background-color: #FFFFFF;
+                line-height: 1.2rem;
+                height: 1.2rem;
+                margin-top: -5px;
+                padding: 0 5px;
+                border-bottom-left-radius: 8px;
+                border-bottom-right-radius: 8px;
+            }
+            .mui-table-view {
+                background-color: #efeff4;
+            }
+        </style>
+</head>
+<body class="home">
+    <header class="mui-bar mui-bar-nav">
+        <h1 class="mui-title">{{ $title }}</h1>
+    </header>
+    <div class="mui-content">
+        <ul class="mui-table-view mui-grid-view" id="lightgallery">
+            @php
+                $ii = 1;
+            @endphp
+            @foreach($photoes as $photo)
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-2" data-responsive="{{ $photo['preview'] }}"  data-src="{{ $photo['original'] }}" data-sub-html="">
+                <a href="javascript:void(0);">
+                    <img class="mui-media-object img-responsive" src="{{ $photo['preview'] }}" style="border-radius: 5px;">
+                    <div class="mui-media-body">{{ $ii }}</div>
+                </a>
+            </li>
+            @php
+                $ii += 1;
+            @endphp
+            @endforeach
+        </ul>
+    </div>
+    <script src="{{ $s }}/js/jquery-1.10.2.js"></script>
+    <script src="{{ $s }}/lightGallery-1.6.11/picturefill.min.js"></script>
+    <script src="{{ $s }}/lightGallery-1.6.11/lightgallery-all.min.js"></script>
+    <script src="{{ $s }}/lightGallery-1.6.11/jquery.mousewheel.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
             $('#lightgallery').lightGallery();
-          });
-        </script>
-    </body>
+        });
+
+        window.onload = function() {
+            if (document.documentElement.scrollHeight <= document.documentElement.clientHeight) {
+                bodyTag = document.getElementsByTagName('body')[0];
+                bodyTag.style.height = document.documentElement.clientWidth / screen.width * screen.height + 'px';
+            }
+            setTimeout(function() {
+                window.scrollTo(0, 1);
+            }, 0);
+        };
+</script>
+</body>
 </html>
