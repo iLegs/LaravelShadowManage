4c9c9f66d0e00e71c88837e823d68b8e2dc4895c
fairyin <fairyin@126.com>
2018-12-20 15:17:13 +0800

    关闭debug模式。

--- resources/views/shadow/main.blade.php
+++ resources/views/shadow/main.blade.php
@@ -93,7 +93,7 @@
             ui:{
                 hideMode:'offsets'
             }, //【可选】hideMode:navTab组件切换的隐藏方式，支持的值有’display’，’offsets’负数偏移位置的值，默认值为’display’
-            debug:true,    // 调试模式 【true|false】
+            debug: false,    // 调试模式 【true|false】
             callback:function(){
                 initEnv();
                 $("#themeList").theme({themeBase:"themes"}); // themeBase 相对于index页面的主题base路径
