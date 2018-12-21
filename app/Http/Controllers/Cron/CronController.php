59b056c9b32c11f686cd822e7d033d8df8ba5d75
fairyin <fairyin@126.com>
2018-12-21 21:26:19 +0800

    优化redis删除图片脚本。

--- app/Http/Controllers/Cron/CronController.php
+++ app/Http/Controllers/Cron/CronController.php
@@ -10,6 +10,7 @@
 namespace App\Http\Controllers\Cron;
 
 use Input;
+use Redis;
 use Exception;
 use Illuminate\Routing\Controller as BaseController;
 
@@ -20,6 +21,39 @@ class CronController extends BaseController
         $s_pass = Input::get('pass', '');
         if ('' == $s_pass || getenv('CRON_SECRET') != $s_pass) {
             throw new Exception("秘钥验证失败！");
+        }
+    }
+
+    /**
+     * 删除 redis 指定的 key。
+     * @param  string $key 键名
+     * @return bool
+     */
+    protected function cleanRedisData($key, $flag = 0)
+    {
+        try {
+            //批量删除
+            if (1 === $flag) {
+                $a_keys = Redis::keys($key . '*');
+                if (!count($a_keys)) {
+                    return false;
+                }
+                Redis::del($a_keys);
+
+                return true;
+            }
+            //普通删除
+            if (Redis::exists($key)) {
+                Redis::del($key);
+
+                return true;
+            }
+
+            return false;
+        } catch (Exception $e) {
+            Log::info('redis del erroor:' . $key . 'msg:' . $e->getMessage());
+
+            return false;
         }
     }
 }
--- app/Http/Controllers/Cron/PhotoDelController.php
+++ app/Http/Controllers/Cron/PhotoDelController.php
@@ -19,40 +19,44 @@ class PhotoDelController extends CronController
 {
     const RDS_KEY = 'photo_del_queue';
 
-    /**
-     * ip 白名单。
-     */
-    public static $a_ips = array(
-    );
+    const RDS_DEL_KEY = 'photoes:album_';
+
+    const NUM = 100;
 
     public function onGet()
     {
-        $s_result = Redis::lpop(static::RDS_KEY);
-        if (!$s_result) {
-            return 'null';
-        }
-        $a_row = json_decode($s_result, true);
-        $o_photo = Photo::find($a_row['pid']);
-        if (!$o_photo || $o_photo->status != 1) {
-            return 'status error';
-        } elseif (!in_array($a_row['ip'], static::$a_ips)) {
-            return 'ip error';
-        }
+        $s_ips = getenv('CRON_IP');
+        $a_ips = explode(',', $s_ips);
         $s_ak = getenv('QINIU_AK');
         $s_sk = getenv('QINIU_SK');
         $s_bucket = getenv('QINIU_SPACE');
         $o_auth = new Auth($s_ak, $s_sk);
         $o_config = new \Qiniu\Config();
         $o_manage = new \Qiniu\Storage\BucketManager($o_auth, $o_config);
-        $err = $o_manage->delete($s_bucket, $o_photo->qn_key);
-        if ($err) {
-            Log::error('cron photo delete error:' . $err);
-
-            return 'error';
+        for ($i = 0; $i < static::NUM; $i++) {
+            $s_result = Redis::lpop(static::RDS_KEY);
+            if (!$s_result) {
+                Log::info('photo delete data null');
+                break;
+            }
+            $a_row = json_decode($s_result, true);
+            $o_photo = Photo::find($a_row['pid']);
+            if (!$o_photo || $o_photo->status != 1) {
+                continue;
+            } elseif (!in_array($a_row['ip'], $a_ips)) {
+                Log::info('photo delete ip error');
+                continue;
+            }
+            $err = $o_manage->delete($s_bucket, $o_photo->qn_key);
+            if ($err) {
+                Log::error('cron photo delete error:' . $err);
+                continue;
+            }
+            $o_photo->status = 0;
+            $o_photo->save();
+            $this->cleanRedisData(static::RDS_DEL_KEY . $o_photo->album_id);
+            Log::info('cron photo delete success:' . $o_photo->qn_key);
         }
-        $o_photo->status = 0;
-        $o_photo->save();
-        Log::info('cron photo delete success:' . $o_photo->qn_key);
 
         return 'success';
     }
