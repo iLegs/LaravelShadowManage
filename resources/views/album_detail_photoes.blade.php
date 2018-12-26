d89f45ff53dfedeaa04d40ab0dcbdd221a2cf157
fairyin <fairyin@126.com>
2018-12-26 11:11:16 +0800

    添加自动推送功能。

--- resources/views/album_detail_photoes.blade.php
+++ resources/views/album_detail_photoes.blade.php
@@ -47,6 +47,17 @@
             var s = document.getElementsByTagName("script")[0];
             s.parentNode.insertBefore(hm, s);
         })();
+        (function(){
+            var bp = document.createElement('script');
+            var curProtocol = window.location.protocol.split(':')[0];
+            if (curProtocol === 'https') {
+                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            } else {
+                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+            }
+            var ss = document.getElementsByTagName("script")[0];
+            ss.parentNode.insertBefore(bp, ss);
+        })();
     </script>
 </body>
 </html>
