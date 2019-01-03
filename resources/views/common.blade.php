41841413dd3cce9834ee1a2518463e17bbc0a639
fairyin <fairyin@126.com>
2019-01-03 20:56:27 +0800

    重新优化首页。

---
+++ resources/views/common.blade.php
@@ -0,0 +1,289 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>iLegs · 时光印象网</title>
+    <meta name="keywords" content="黑丝，美腿，制服，写真，Ligui，Beautyleg，iLegs，时光印象，时光印象网">
+    <meta name="description" content="时光印象网致力于图片分享，建设最高清、最专业的美图分享网站。">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="full-screen" content="yes">
+    <meta name="x5-fullscreen" content="true">
+    <link rel="stylesheet" href="{{ $s }}/bootstrap-4.1.3/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ $s }}/dripicons/webfont.css">
+    <link rel="stylesheet" href="{{ $s }}/css/web/style.css">
+    <style type="text/css">
+    @media(max-width: 600px){
+        .container.banner {
+            padding: 0 !important;
+            margin: 0 !important;
+        }
+    }
+    .albums{
+        margin-top: 1rem;
+    }
+    .icon {
+        color: #FFFFFF;
+    }
+    @media (max-width: 960px){
+        .container {
+            padding: 0;
+        }
+    }
+    @media (max-width: 768px){
+    .col-6.col-md-3.col-xl-3 {
+            padding: .1rem;
+        }
+    }
+    @media (min-width: 768px){
+        .col-6.col-md-3.col-xl-3 {
+            padding: .2rem;
+        }
+    }
+    @media (min-width: 1200px){
+        .col-6.col-md-3.col-xl-3 {
+            padding: .4rem;
+        }
+    }
+    .card {
+        border-radius: .5rem;
+    }
+    .card img {
+        border-top-left-radius: .5rem;
+        border-top-right-radius: .5rem;
+    }
+    </style>
+</head>
+<body data-csrf-token="{{ csrf_token() }}">
+    <header class="container-fluid iheader">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="/">iLegs</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="#">首页 <span class="sr-only">(current)</span></a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <div class="container banner">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="http://leg.imcn.vip/2015/No.1175_Miso_17.jpg-crossrange_fhd?e=1546519025&token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:M5ERBAcCWfh3zTjB9fN8Y63vKfA=" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+        <h5>卡萨丁看上的客家话.</h5>
+        <p>卡萨丁看上的客家话..</p>
+        </div>
+        </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+        </a>
+        </div>
+    </div>
+    <div class="container">
+        <div class="albums">
+        <div class="row album-header" style="margin-bottom: 1rem;">
+            <div class="col-6" style="text-align: left;">
+                <a class="btn btn-primary icon" href="#" data-icon="&#xe010;" role="button" style="border: none;background-color: #9966FF;">&nbsp;Beautyleg</a>
+            </div>
+            <div class="col-6" style="text-align: right;">
+                <a class="btn btn-primary icon" href="#" data-icon="^" role="button" style="border: none;background-color: #9966FF;">&nbsp;查看全部</a>
+            </div>
+        </div>
+        <div class="row" style="padding: 0;margin: 0;">
+            <div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="albums">
+        <div class="row album-header" style="margin-bottom: 1rem;">
+            <div class="col-6" style="text-align: left;">
+                <a class="btn btn-primary icon" href="#" data-icon="&#xe010;" role="button" style="border: none;background-color: #9966FF;">&nbsp;Beautyleg</a>
+            </div>
+            <div class="col-6" style="text-align: right;">
+                <a class="btn btn-primary icon" href="#" data-icon="^" role="button" style="border: none;background-color: #9966FF;">&nbsp;查看全部</a>
+            </div>
+        </div>
+        <div class="row" style="padding: 0;margin: 0;">
+            <div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div><div class="col-6 col-md-3 col-xl-3">
+                <div class="card">
+                    <a href="/album/detail/707.html">
+                        <img class="img-fluid" src="http://leg.imcn.vip/cover/20190102/5c2c50fbe6e1d.jpg-mobile_cover?e=1546627368&amp;token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:VrsiHrQSV-lAkUV4j-eIzgaME6c=" alt="[Beautyleg]2018.12.28 No.1705 Tina">
+                    </a>
+                    <div class="card-body album-title">
+                        <p class="card-text text-truncate text-center">[Beautyleg]2018.12.28 No.1705 Tina</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <script src="{{ $s }}/bootstrap-4.1.3/jquery-3.3.1.min.js"></script>
+    <script src="{{ $s }}/bootstrap-4.1.3/bootstrap.min.js"></script>
+    <script src="{{ $s }}/bootstrap-4.1.3/popper.min.js"></script>
+    <script type="text/javascript">
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?3d0f46b4bb96b324fee66680619b38ec";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+</body>
+</html>
